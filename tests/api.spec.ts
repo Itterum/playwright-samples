6e742c03f641ac7855ae9fff58fc644ad535211b
itterum <ivan.lyashenko.it@gmail.com>
2025-04-24 09:50:57 +0300

    wtf?

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,5 +1,10 @@
-import {test, expect} from '@playwright/test';
+import {test, expect} from '../support/fixtures/api.fixture';
+import {UserResponseDTO, UserRequestDTO} from '../support/dto/api.dto';
 
-test('api', async ({page}) => {
-
+test.describe('User', () => {
+  test('CRUD user', async ({baseApi}) => {
+    await test.step('POST /user', async () => {
+      const user: UserRequestDTO = {};
+    });
+  });
 });
